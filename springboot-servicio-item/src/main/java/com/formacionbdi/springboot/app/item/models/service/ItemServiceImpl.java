d659ee9d472e818fbac4de88252cee8e4a956e06
H <hruiz17@hotmail.com>
2023-08-02 19:38:37 -0500

    se agrega el balanceador ribbon para feign y restTemplate

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -21,7 +21,7 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public List<Item> findAll() {
-		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://servicio-productos/listar", Producto[].class));
 		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
@@ -29,7 +29,7 @@ public class ItemServiceImpl implements ItemService {
 	public Item findById(Long id, Integer cantidad) {
 		Map<String, String> parametros = new HashMap<String, String>();
 		parametros.put("id", id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, parametros);
+		Producto producto = clienteRest.getForObject("http://servicio-productos/ver/{id}", Producto.class, parametros);
 		return new Item(producto, cantidad);
 	}
 
