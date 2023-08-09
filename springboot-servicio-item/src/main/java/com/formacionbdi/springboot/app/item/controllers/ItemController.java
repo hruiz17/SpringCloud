3459f5a77f108e2bb299b4452571d376bbe45425
H <hruiz17@hotmail.com>
2023-08-09 17:55:01 -0500

    se agrega el micro config-server, configuracion y actuator-refresh

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,13 +1,20 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,10 +28,14 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
@@ -32,6 +43,9 @@ public class ItemController {
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre,
@@ -94,6 +108,22 @@ public class ItemController {
 		item.setCantidad(cantidad);
 		item.setProducto(producto);
 		return CompletableFuture.supplyAsync(() -> item);
+	}
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+		logger.info(texto);
+
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+			return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 //	Resilience4J
