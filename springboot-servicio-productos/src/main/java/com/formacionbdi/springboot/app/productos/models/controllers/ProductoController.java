22386494864ffa272585765346a6964c01aa9b79
H <hruiz17@hotmail.com>
2023-08-10 07:15:24 -0500

    se agrega la libreria commons para clases en común

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/controllers/ProductoController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
 
 @RestController
